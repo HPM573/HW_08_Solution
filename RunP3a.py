c0bc6b4881f030be2953a7700eb78fe615207227
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2023-04-02 09:14:13 -0400

    Update RunP3a.py

--- RunP3a.py
+++ RunP3a.py
@@ -7,7 +7,7 @@ print('How much is the casino owner expected to pay to a gambler'
       '(if negative, it means that the gambler is paying to the casino owner):', setOfGames.statRewards.get_mean())
 print('The 95% CI is', setOfGames.statRewards.get_t_CI(0.05))
 
-print('\nWe need a steady-state simulation for this perspective.')
-print('We are able to rely on the Law of Large Numbers to make inference about '
+print('Since the casio owner gets to play this game many time', 
+      'we are able to rely on the Law of Large Numbers to make inference about '
       'home much the casino owner could make from each gambler.')
 print('This lets us use the sample mean and confidence intervals for interpretation.')
